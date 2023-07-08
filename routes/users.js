2fb5e18575c2713731a853c5fe12ac4f03137653
ben.miller <ben.miller@siemens.com>
2023-07-08 19:57:50 +0100

    Reverted to require statements, connected DB and bug fixes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,44 @@
-import { query } from '../db/index';
+const db = require('../db');
 
-const getUsers = async () => {
-    const res = await query('SELECT * FROM users');
-    return res.rows;
+const getUsers = (request, response) => {
+    db.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+};
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id);
+    console.log(`Id is set to: ${id}`);
+
+    db.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            console.log('2');
+            throw error;
+        }
+        response.status(200).json(results.rows);
+        console.log('3');
+    });
+
+    console.log('End of request');
+};
+
+const createUser = (request, response) => {
+    const { username, email, password_hash, first_name, last_name, address } = request.body
+
+    db.query('INSERT INTO users (username, email, password_hash, first_name, last_name, address) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+        [username, email, password_hash, first_name, last_name, address], (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+        })
+};
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
 };
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 /*
  * Package Imports
  */
-import express from 'express';
-import session from "express-session";
-import passport from "passport";
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const Users = require('./routes/users');
 
 dotenv.config();
 
 const server = express();
-
-export default server;
-
 const PORT = process.env.PORT || 4001;
 
-// Add middware for parsing request bodies here:
 server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
-// This conditional is here for testing purposes:
-  // Add your code to start the server listening at PORT below:
-  server.listen(PORT, () => {
-    console.log(`Server listening on Port ${PORT}`)
-  });
+server.get('/users', Users.getUsers);
+server.get('/users/:id', Users.getUserById);
+server.post('/users', Users.createUser);
+
+server.listen(PORT, () => { console.log(`Server listening on Port ${PORT}`) });
